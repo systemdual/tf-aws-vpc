eea43df0e60f89bd582709549dcb8769bc83f333
Mauro Fernández Quiñones <maurofq1991@gmail.com>
2022-11-13 15:06:51 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,19 +5,3 @@ output "igw_id" {
 output "name" {
   value = module.vpc.name
 }
-
-output "private_subnets" {
-  value = module.vpc.private_subnets
-}
-
-output "private_subnets_cidr_blocks" {
-  value = module.vpc.private_subnets_cidr_blocks
-}
-
-output "public_subnets" {
-  value = module.vpc.private_subnets
-}
-
-output "public_subnets_cidr_blocks" {
-  value = module.vpc.private_subnets_cidr_blocks
-}
